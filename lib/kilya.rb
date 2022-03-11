ac3dd17baea784cc31499447330a4078e8c76ced
kilya <ilya.kondratyev@kupibilet.ru>
2022-03-11 22:31:55 +0100

    add version 0.0.6

--- lib/kilya.rb
+++ lib/kilya.rb
@@ -1,6 +1,15 @@
+# main pont
 require 'services/string_builder'
 
 class Kilya
+  # It's my first simple gem
+  #
+  # Example:
+  #   >> Kilya.reverse("abcd")
+  #   => "dcba"
+  #
+  # Arguments:
+  #   line: (String)
   class << self
     include Services::StringBuilder
   end
