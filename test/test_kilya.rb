c22e8e1df7589dcd8857dcf4c6d0bd5cd8e6fbd2
kilya <ilya.kondratyev@kupibilet.ru>
2022-03-11 22:17:11 +0100

    [0001] Add test for empty request

--- test/test_kilya.rb
+++ test/test_kilya.rb
@@ -3,6 +3,10 @@ require 'kilya'
 
 class TestKilya < Minitest::Test
   def test_reverse_string
-    assert_equal("aaasss", Kilya.reverse("ssssaa"))
+    assert_equal("aaasss", Kilya.reverse("sssaaa"))
+  end
+
+  def test_not_empty_reverse
+    assert_equal("nothing to reverse", Kilya.reverse(nil))
   end
 end
